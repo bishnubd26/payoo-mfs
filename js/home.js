23771ff8fb7b75eca6157f66d6814331c858e2da
bishnubd26 <bishnunath820@gmail.com>
2024-10-12 19:17:48 +0600

    added partially the add money event handler

--- js/home.js
+++ js/home.js
@@ -1,1 +1,8 @@
-console.log('home added')
+// add money to the account
+document.getElementById('btn-add-money').addEventListener('click', function (event) {
+  event.preventDefault();
+
+  const addMoneyInput = document.getElementById('input-add-money').value;
+  const pinNumberInput = document.getElementById('input-pin-number').value;
+  console.log(addMoneyInput, pinNumberInput)
+})
