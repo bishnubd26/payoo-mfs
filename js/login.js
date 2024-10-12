a17666fafcecf5a39a9376ea569958b28955bfd9
bishnubd26 <bishnunath820@gmail.com>
2024-10-12 12:55:45 +0600

    handle login and redirect to the home page

--- js/login.js
+++ js/login.js
@@ -5,5 +5,12 @@ document.getElementById('btn-login').addEventListener('click', function (event) 
   console.log("login btn clicked")
   
   const phoneNumber = document.getElementById('phone-number').value;
-  console.log(phoneNumber);
+  const pinNumber = document.getElementById('pin-number').value;
+  if (phoneNumber === '5' && pinNumber === '1234') {
+    console.log('You are logged in');
+    window.location.href = 'home.html';
+  }
+  else {
+    alert('Wrong phone number or pin');
+  }
 })
